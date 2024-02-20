c2b839e33ee14fd910e59f440ffc3de20fc796ca
LZB-Dev <unitygamemaker7@gmail.com>
2024-02-20 08:30:51 +0100

    first commit

---
+++ Assets/_Scripts/Testfield/Units Classes/Legs.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Legs : MonoBehaviour
+{
+    [SerializeField] protected int weight;
+    [SerializeField] protected float acceleration;
+    [SerializeField] protected float speedLimit;
+}
