08ba85cfddaa89aac887ebb6922055246b7b5b3a
LZB-Dev <unitygamemaker7@gmail.com>
2024-09-29 07:27:15 +0200

    Health/Shield Bar rework + added Energy Bar
    Major HUD rework:
    - Health/Shield Bar have more advanced look (added sprites, gradients, background sprite and slightly transperent filler).
    - TextMeshPro integrated into HP/SP bar, but it is mostly for debugging. Bar looks better without numbers.
    - Updated script for more percise configure of new HUD. Added Width Offset of fillers and setting to either update shield position or not.
    - Added round Energy Bar and logic for it.
    - A bit of brainless performance optimisation by unchecking most image checkboxes.

--- Assets/HPSize.cs
+++ Assets/HPSize.cs
@@ -5,19 +5,30 @@ using UnityEngine.UI;
 using TMPro;
 public class HPSize : MonoBehaviour
 {
+    [Header("Refferences")]
+    [SerializeField] RectTransform Bar;
     [SerializeField] Slider AreaHealthSlider, AreaShieldSlider;
+    [SerializeField] Image EnergyBar;
     [SerializeField] TextMeshProUGUI TextHealth, TextShield;
-    private RectTransform AreaHealth, AreaShield;
-    private RectTransform Bar;
 
+    private RectTransform AreaHealth, AreaShield;
+    [Header("Stats")]
+    public float maxEnergy;
     public int maxHealth, maxShield;
     private int currentHealth, currentShield;
+    private float currentEnergy;
+
+    [Space(20)]
+    [SerializeField] [Tooltip("Updates second bar`s position according to first one fill amount")] bool UpdatePosition = false;
+    [SerializeField][Tooltip("Enables width offset for HP and SP Bars")] private bool EnableOffset;
+    [SerializeField] private float Offset;
+
+
 
     private void Awake()
     {
         AreaHealth = AreaHealthSlider.GetComponentInParent<RectTransform>();
         AreaShield = AreaShieldSlider.GetComponentInParent<RectTransform>();
-        Bar = GetComponent<RectTransform>();
     }
 
     private void Start()
@@ -29,6 +40,7 @@ public class HPSize : MonoBehaviour
     {
         currentHealth = maxHealth;
         currentShield = maxShield;
+        currentEnergy = maxEnergy;
         AreaHealthSlider.maxValue = maxHealth;
         AreaShieldSlider.maxValue = maxShield;
         UpdateBars();
@@ -39,26 +51,30 @@ public class HPSize : MonoBehaviour
     private void UpdateAreaPosition()
     {
         float HealthWidth = Bar.rect.width / 100f * (100f / (AreaHealthSlider.maxValue + AreaShieldSlider.maxValue) * AreaHealthSlider.maxValue);
-        AreaShield.localPosition = new Vector2(HealthWidth / AreaHealthSlider.maxValue * AreaHealthSlider.value, 0);
+        AreaShield.localPosition = new Vector2(HealthWidth / AreaHealthSlider.maxValue * AreaHealthSlider.value - Offset * 2, 0);
         //Debug.Log($"AS pos = {AreaShield.localPosition} - x = {AreaShield.localPosition.x}, y = {AreaShield.localPosition.y}, z = {AreaShield.localPosition.z}");
         
     }
 
     private void BarResize()
     {
-        float HealthWidth = Bar.rect.width / 100f * (100f / (AreaHealthSlider.maxValue + AreaShieldSlider.maxValue) * AreaHealthSlider.maxValue);
-        AreaHealth.sizeDelta = new Vector2(HealthWidth, AreaHealth.sizeDelta.y);
+        float HealthWidth = Bar.rect.width / (AreaHealthSlider.maxValue + AreaShieldSlider.maxValue) * AreaHealthSlider.maxValue;
         float ShieldWidth = Bar.rect.width - HealthWidth;
+        if (EnableOffset) { HealthWidth += Offset; ShieldWidth += Offset; }
+        AreaHealth.sizeDelta = new Vector2(HealthWidth, AreaHealth.sizeDelta.y);
         AreaShield.sizeDelta = new Vector2(ShieldWidth, AreaShield.sizeDelta.y);
         //Debug.Log($"{Bar.rect.width} / 100 * ( 100 / ({AreaHealthSlider.maxValue} + {AreaShieldSlider.maxValue}) * {AreaHealthSlider.maxValue}) = {HealthWidth}" +
         //    $"\nBarWidth / 100 * ((AHS-max + ASS-max) / 100 * AHS-max) = HealthWidth");
-        AreaShield.localPosition = new Vector2(HealthWidth / AreaHealthSlider.maxValue * AreaHealthSlider.value, 0);
+        AreaShield.localPosition = new Vector2(HealthWidth - Offset * 2, 0); //Remember to change X pivot of parent to 0
+        //Debug.Log($"{HealthWidth} / {AreaHealthSlider.maxValue} * {AreaHealthSlider.value} = {AreaShield.localPosition}" +
+        //    $"\nHealthWidth / AHS.max * AHS.value = AS.pos");
     }
 
     private void UpdateBars()
     {
         AreaHealthSlider.value = currentHealth;
         AreaShieldSlider.value = currentShield;
+        EnergyBar.fillAmount = 1f / maxEnergy * currentEnergy;
     }
 
     private void UpdateText()
@@ -69,12 +85,12 @@ public class HPSize : MonoBehaviour
 
     public enum TargetType
     {
-        Health, Shield
+        Health, Shield, Energy
     }
 
     public enum Action
     {
-        Damage, Restore
+        Lose, Restore
     }
 
     public void Interaction(TargetType target, Action action, int amount)
@@ -84,7 +100,7 @@ public class HPSize : MonoBehaviour
             case TargetType.Health:
                 switch (action)
                 {
-                    case Action.Damage:
+                    case Action.Lose:
                         currentHealth -= amount;
                         if (currentHealth < 0) { currentHealth = 0; }
                         break;
@@ -97,7 +113,7 @@ public class HPSize : MonoBehaviour
             case TargetType.Shield:
                 switch (action)
                 {
-                    case Action.Damage:
+                    case Action.Lose:
                         currentShield -= amount;
                         if (currentShield < 0) { currentShield = 0; }
                         break;
@@ -107,18 +123,34 @@ public class HPSize : MonoBehaviour
                         break;
                 }
                 break;
+            case TargetType.Energy:
+                switch (action)
+                {
+                    case Action.Lose:
+                        currentEnergy -= amount;
+                        if (currentEnergy < 0) { currentEnergy = 0; }
+                        break;
+                    case Action.Restore:
+                        currentEnergy += amount;
+                        if (currentEnergy > maxEnergy) { currentEnergy = maxEnergy; }
+                        break;
+                }
+                break;
         }
         UpdateText();
         UpdateBars();
-        //UpdateAreaPosition();
+        if (UpdatePosition) { UpdateAreaPosition(); }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad1)) { Interaction(TargetType.Health, Action.Damage, 20); }
+        if (Input.GetKeyDown(KeyCode.Keypad1)) { Interaction(TargetType.Health, Action.Lose, 20); }
         else if (Input.GetKeyDown(KeyCode.Keypad2)) { Interaction(TargetType.Health, Action.Restore, 20); }
-        else if (Input.GetKeyDown(KeyCode.Keypad4)) { Interaction(TargetType.Shield, Action.Damage, 20); }
+        else if (Input.GetKeyDown(KeyCode.Keypad4)) { Interaction(TargetType.Shield, Action.Lose, 20); }
         else if (Input.GetKeyDown(KeyCode.Keypad5)) { Interaction(TargetType.Shield, Action.Restore, 20); }
+        else if (Input.GetKeyDown(KeyCode.Keypad7)) { Interaction(TargetType.Energy, Action.Lose, 20); }
+        else if (Input.GetKeyDown(KeyCode.Keypad8)) { Interaction(TargetType.Energy, Action.Restore, 20); }
+
     }
 
     #region old code
